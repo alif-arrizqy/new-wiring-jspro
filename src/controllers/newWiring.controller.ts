bd11007c3f755857c42369578a1d8bbca2d5faf1
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-10-31 16:09:31 +0700

    create controller and repository

---
+++ src/controllers/newWiring.controller.ts
@@ -0,0 +1,107 @@
+import { Request, Response } from "express";
+import NewWiring from "../models/newWiring.model";
+import newWiringRepository from "../repositories/newWiring.repository";
+
+export default class NewWiringController {
+  async create(req: Request, res: Response) {
+    if (!req.body.sites_id) {
+      res.status(400).send({
+        message: "data can not be empty",
+      });
+      return;
+    }
+
+    try {
+      const newWiring: NewWiring = req.body;
+      if (!newWiring.status) newWiring.status = true;
+      if (!newWiring.source_modbus) newWiring.source_modbus = "serial";
+
+      const resp = await newWiringRepository.save(newWiring);
+      res.status(201).send(resp);
+    } catch (err) {
+      res.status(500).send({
+        message: "failed to create data",
+      });
+    }
+  }
+
+  async findAll(req: Request, res: Response) {
+    try {
+      const response = await newWiringRepository.retrieveAll();
+      res.status(200).send(response);
+    } catch (err) {
+      res.status(500).send({
+        message: "some error occured while retrieving data",
+      });
+    }
+  }
+
+  async findOne(req: Request, res: Response) {
+    const sites_id: string = req.params.sites_id;
+    try {
+      const response = await newWiringRepository.retrieveBySitesId(sites_id);
+      if (response) res.status(200).send(response);
+      else
+        res.status(404).send({
+          message: `cannot find data with site ${sites_id}`,
+        });
+    } catch (err) {
+      res.status(500).send({
+        message: `error find data with site ${sites_id}`,
+      });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    let data: NewWiring = req.body;
+    data.id = parseInt(req.params.id);
+
+    try {
+      const resp = await newWiringRepository.update(data);
+      if (resp == 1) {
+        res.send({ message: "data was updated successfully" });
+      } else {
+        res.send({ message: `cannot update data with id ${data.id}` });
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .send({ message: `error updating data with id  ${data.id}` });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    const id: number = parseInt(req.params.id);
+
+    try {
+      const num = await newWiringRepository.delete(id);
+
+      if (num == 1) {
+        res.send({
+          message: "data was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete data with id=${id}`,
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        message: `could not delete Tutorial with id ${id}`,
+      });
+    }
+  }
+
+  async deleteAll(req: Request, res: Response) {
+    try {
+      const num = await newWiringRepository.deleteAll();
+      if (num == 1) {
+        res.send({ message: `data were deleted successfully!` });
+      }
+    } catch (err) {
+      res.status(500).send({
+        message: "Some error occurred while removing all tutorials.",
+      });
+    }
+  }
+}
---
+++ src/repositories/newWiring.repository.ts
@@ -0,0 +1,79 @@
+import NewWiring from "../models/newWiring.model";
+
+interface INewWiring {
+  save(newWiring: NewWiring): Promise<NewWiring>;
+  retrieveAll(): Promise<NewWiring[]>;
+  retrieveBySitesId(sites_id: string): Promise<NewWiring | null>;
+  update(tutorial: NewWiring): Promise<number>;
+  delete(id: number): Promise<number>;
+  deleteAll(): Promise<number>;
+}
+
+class NewWiringRepository implements INewWiring {
+  async save(newWiring: NewWiring): Promise<NewWiring> {
+    try {
+      return await NewWiring.create({
+        sites_id: newWiring.sites_id,
+        status: newWiring.status,
+        source_modbus: newWiring.source_modbus.toLowerCase(),
+        notes: newWiring.notes,
+      });
+    } catch (err) {
+      throw new Error(`Failed to create data, ${err}`);
+    }
+  }
+
+  async retrieveAll(): Promise<NewWiring[]> {
+    try {
+      return await NewWiring.findAll();
+    } catch (err) {
+      throw new Error(`Failed to retrieve all data, ${err}`);
+    }
+  }
+
+  async retrieveBySitesId(sites_id: string): Promise<NewWiring | null> {
+    try {
+      return await NewWiring.findOne({ where: { sites_id: sites_id } });
+    } catch (err) {
+      throw new Error(`Failed to retrieve data, ${err}`);
+    }
+  }
+
+  async update(newWiring: NewWiring): Promise<number> {
+    const { id, sites_id, status, source_modbus, notes } = newWiring;
+    try {
+      const response = await NewWiring.update(
+        { sites_id, status, source_modbus, notes },
+        { where: { id: id } }
+      );
+      return response[0];
+    } catch (err) {
+      throw new Error(`failed to update data, ${err}`);
+    }
+  }
+
+  async delete(id: number): Promise<number> {
+    try {
+      const response = await NewWiring.destroy({
+        where: { id: id },
+      });
+
+      return response;
+    } catch (err) {
+      throw new Error(`Failed to delete data, ${err}`);
+    }
+  }
+
+  async deleteAll(): Promise<number> {
+    try {
+      return NewWiring.destroy({
+        where: {},
+        truncate: false,
+      });
+    } catch (err) {
+      throw new Error(`Failed to delete data, ${err}`);
+    }
+  }
+}
+
+export default new NewWiringRepository();
