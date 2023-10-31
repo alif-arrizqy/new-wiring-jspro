0ebce58f45a93e2598e2f4f8b6a00943f1d4bcfd
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-10-31 16:08:44 +0700

    database config

---
+++ src/config/db.config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USERNAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  PORT: process.env.DB_PORT,
+  DIALECT: process.env.DB_DIALECT,
+  pool: {
+    max: 25,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+export default config;
---
+++ src/db/index.ts
@@ -0,0 +1,36 @@
+import { Sequelize } from "sequelize-typescript";
+import config from "../config/db.config";
+import NewWiring from "../models/newWiring.model";
+
+class Database {
+  public sequelize: Sequelize | undefined;
+
+  constructor() {
+    this.connectToDatabase();
+  }
+
+  private async connectToDatabase() {
+    const uri = `${config.DIALECT}://${config.USER}:${config.PASSWORD}@${config.HOST}:${config.PORT}/${config.DB}`;
+    this.sequelize = new Sequelize(uri, {
+      logging: false,
+      pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle,
+      },
+      models: [NewWiring],
+    });
+
+    await this.sequelize
+      .authenticate()
+      .then(() => {
+        console.log("Connection has been established successfully.");
+      })
+      .catch((err) => {
+        console.error("Unable to connect to the Database:", err);
+      });
+  }
+}
+
+export default Database;
